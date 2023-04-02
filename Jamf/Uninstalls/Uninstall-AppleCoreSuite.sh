7ea95de7f7e31f269faf32ff8c65798b51abf3e7
MaverickMidori <119835847+maverickmidori@users.noreply.github.com>
2023-04-03 06:55:33 +1000

    Added Logic & Refined
    Check if Directory Exists; if so forcibly remove directory.
    Created function to remove repetitive tasks, improve scalability.

--- Jamf/Uninstalls/Uninstall-AppleCoreSuite.sh
+++ Jamf/Uninstalls/Uninstall-AppleCoreSuite.sh
@@ -7,8 +7,22 @@
 ### Use: Generic / Entity-Agnostic              ###
 ### Notes: Only Removes Core 3 Applications     ###
 ###################################################
-
-# Remove - Remove Forcibly the following Applications
-rm -rf /Applications/Pages.app
-rm -rf /Applications/Keynote.app
-rm -rf /Applications/Numbers.app
+DirPath=/Applications/
+appName=(
+    'Pages'
+    'Keynote'
+    'Numbers'
+)
+function UninstallApp() {
+    declare -a appName=("${!1}")
+    for app in "${appName[@]}"
+    do
+        if [ -d $DirPath$app".app" ]; then
+            rm -rf $DirPath$app".app" && echo "Uninstalling "$app".app"
+        else
+            echo $app" is not installed, proceeding with script..."
+        fi
+    done
+}
+# Invoke Function CloseApp with Array appName
+UninstallApp appName[@]
